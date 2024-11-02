2190f90aa73d78b6c12c08bb068ab307a2d8e917
Alya Mazurkevich <alyamazurkevich99@mail.ru>
2024-11-03 02:04:12 +0300

    develop1

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class BunTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+@RunWith(Parameterized.class)
+public class BunTest {
+
+    public String name;
+    public float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] bun() {
+        return new Object[][]{
+                {"pers", 0},
+                {"black bun", 100},
+                {"white bun", 200},
+                {"red bun", 300},
+                {null, 1},
+
+        };
+    }
+
+    @Test
+    public void checkBunName(){
+        Bun bunTest = new Bun(name, price);
+        String actualBunName = bunTest.getName();
+        Assert.assertEquals(name, actualBunName);
+
+    }
+
+    @Test
+    public void checkBunPrice(){
+        Bun bunTest = new Bun(name, price);
+        float actualBunPrice = bunTest.getPrice();
+        Assert.assertEquals(price, actualBunPrice, 0);
+
+    }
 }
